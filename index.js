59b81dd97c9142c3cf2180612acc87b77c89dd40
uvcanaditya <uvcan.aditya@gmail.com>
2023-11-01 00:58:50 +0530

    Add controller , views , route

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port=8000;
 //Use express router
 app.use('/',require('./routes'));
 
-
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
